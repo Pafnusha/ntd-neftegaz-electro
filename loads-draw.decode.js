7e8e0a251c874071a0d29a22f80832bba2bfcc44
Vladimir <unggbkru@gmail.com>
2026-09-23 00:27:47 +0300

    feat(loads): loads-draw split gzip decode loader

--- loads-draw.decode.js
+++ loads-draw.decode.js
@@ -1,5 +1,7 @@
 (async function(){
-  var b64=await (await fetch('loads-draw.gz.b64')).text();
+  var p0=await (await fetch('loads-draw.gz.b64.0')).text();
+  var p1=await (await fetch('loads-draw.gz.b64.1')).text();
+  var b64=(p0+p1).trim();
   var bin=Uint8Array.from(atob(b64), function(c){return c.charCodeAt(0);});
   var ds=new DecompressionStream('gzip');
   var stream=new Response(bin).body.pipeThrough(ds);
